dbb6f888391021ec6bfb0b68cbed7a77b44b20d1
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-03 10:32:33 +0600

    feat: Replace YOLO with Inference API for object detection
    This commit replaces the local YOLO model with the Inference API for object detection. This change utilizes  and  for processing and updates the  creation accordingly.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import cv2
 import supervision as sv
-from ultralytics import YOLO
+# from ultralytics import YOLO
+from inference import get_model
 
-model = YOLO("yolov8n.pt")
+""" model = YOLO("yolov8n.pt")
 image = cv2.imread("resources/images/traffic-1.webp")
 results = model(image)[0]
-detections = sv.Detections.from_ultralytics(results)
+detections = sv.Detections.from_ultralytics(results) """
+
+model = get_model(model_id="yolov8n-640")
+image = cv2.imread("resources/images/traffic-1.webp")
+results = model.infer(image)[0]
+detections = sv.Detections.from_inference(results)
 
 box_annotator = sv.BoxAnnotator()
 label_annotator = sv.LabelAnnotator()
@@ -15,7 +21,6 @@ annotated_image = box_annotator.annotate(
 annotated_image = label_annotator.annotate(
     scene=annotated_image, detections=detections)
 
-# Show the result
 cv2.imshow("Traffic Detection", annotated_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
