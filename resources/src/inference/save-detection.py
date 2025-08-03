6b31a151399208a735a73aa940c80d020f8196c8
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-03 10:13:34 +0600

    Fix: Save detection results to the results directory
    The script now saves the detection results to the  directory to improve project organization.

--- resources/src/inference/save-detection.py
+++ resources/src/inference/save-detection.py
@@ -4,7 +4,7 @@ from inference import get_model
 model = get_model(model_id="yolov8n-640")
 frames_generator = sv.get_video_frames_generator("../../videos/traffic-1.mp4")
 
-with sv.CSVSink("../../videos/traffic.csv") as sink:
+with sv.CSVSink("../../videos/results/traffic.csv") as sink:
     for frame in frames_generator:
         results = model.infer(frame)[0]
         detections = sv.Detections.from_inference(results)
