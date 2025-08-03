18defaef2cbbb8d358dd74d8fd27d6140a6a713f
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-03 10:39:34 +0600

    feat: Comment out inference code
    This commit temporarily comments out the inference code related to the external  module. This allows the code to run without relying on the module, likely for testing or debugging purposes.

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import cv2
 import supervision as sv
 from ultralytics import YOLO
+# from inference import get_model
 
 model = YOLO("yolov8n.pt")
 image = cv2.imread("resources/images/traffic-1.webp")
-
 results = model(image)[0]
 detections = sv.Detections.from_ultralytics(results)
 
-class_names = model.model.names  # ← mapping class_id to name
-
-labels = [
-    f"{class_names[class_id]} {confidence:.2f}"
-    for class_id, confidence in zip(detections.class_id, detections.confidence)
-]
+""" model = get_model(model_id="yolov8n-640")
+image = cv2.imread("resources/images/traffic-1.webp")
+results = model.infer(image)[0]
+detections = sv.Detections.from_inference(results) """
 
 box_annotator = sv.BoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 
-annotated_image = box_annotator.annotate(scene=image, detections=detections)
-annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+annotated_image = box_annotator.annotate(
+    scene=image, detections=detections)
+annotated_image = label_annotator.annotate(
+    scene=annotated_image, detections=detections)
 
 cv2.imshow("Traffic Detection", annotated_image)
 cv2.waitKey(0)
