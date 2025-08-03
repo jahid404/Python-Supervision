7e8909a5a702004d99b2d0f2db761276072136e0
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-03 10:49:34 +0600

    feat: Display class name and confidence in bounding box labels
    This commit modifies the label annotator to display both the class name and confidence score for each detection.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import supervision as sv
 from ultralytics import YOLO
 
-# Load model and process image
 model = YOLO("yolov8n.pt")
 image = cv2.imread("resources/images/traffic-1.webp")
 results = model(image)[0]
@@ -11,9 +10,14 @@ detections = sv.Detections.from_ultralytics(results)
 box_annotator = sv.BoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 
-annotated_image = box_annotator.annotate(scene=image.copy(), detections=detections)
-annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
+# custom labels
+labels = [
+    f"{class_name} {confidence:.2f}"
+    for class_name, confidence
+    in zip(detections['class_name'], detections.confidence)
+]
 
-# Save image to verify annotations
+annotated_image = box_annotator.annotate(scene=image.copy(), detections=detections)
+annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+
 cv2.imwrite("output.jpg", annotated_image)
-print("Saved annotated image to output.jpg")
