b8ca898df30bc96a30ea90c3ad60c9b9293c1914
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-08-03 10:51:20 +0600

    feat: Implement basic YOLOv8 object detection script
    This commit adds a script (detect.py) that performs object detection using YOLOv8 on a sample image and saves the annotated image to output.jpg. It utilizes the ultralytics and supervision libraries.

---
+++ src/yolo/detect.py
@@ -0,0 +1,23 @@
+import cv2
+import supervision as sv
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+image = cv2.imread("resources/images/traffic-1.webp")
+results = model(image)[0]
+detections = sv.Detections.from_ultralytics(results)
+
+box_annotator = sv.BoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+# custom labels
+labels = [
+    f"{class_name} {confidence:.2f}"
+    for class_name, confidence
+    in zip(detections['class_name'], detections.confidence)
+]
+
+annotated_image = box_annotator.annotate(scene=image.copy(), detections=detections)
+annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+
+cv2.imwrite("output.jpg", annotated_image)
